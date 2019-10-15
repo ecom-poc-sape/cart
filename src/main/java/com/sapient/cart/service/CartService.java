82e2665c1ec1f65f27c4046d9696038352ccb81e
Suresh <suresh.kb4@gmail.com>
2019-10-15 12:25:14 +0530

    Update CartService.java

--- src/main/java/com/sapient/cart/service/CartService.java
+++ src/main/java/com/sapient/cart/service/CartService.java
@@ -1,5 +1,7 @@
 package com.sapient.cart.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -40,7 +42,7 @@ public class CartService {
 
 	}
 
-	public CartItems getAllCartItems(String customerId) {
+	public List<String> getAllCartItems(String customerId) {
 		return kafkaUtil.getAllCartItems(customerId);
 	}
 	
